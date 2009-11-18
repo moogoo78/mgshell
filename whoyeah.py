f80e988fd15806a042fadf51fac1392502f50031
moogoo <moogoo@monki.(none)>
2009-11-18 19:32:42 +0800

    refactor whoyeah, add option parser, can ignore case

--- whoyeah.py
+++ whoyeah.py
@@ -3,50 +3,41 @@
 
 """
 WhoYeah(虎爺) - a grep like string finder in Python
-(bad icon)
-                  ...
-        . ........   ..
-        ..            ..
-       .. ..         .. ..
-     ..    ..     ...   ..
-     .   ... .. ..    ....
-     .   ....     ... .
-     ..       .   .......
-      .      ... . ..   .
-     ..   ......... .....
-     ... ........ ...
 
+*inspired:*
+  - grep-ack - a perl grep-like
+  - grin from http://pypi.python.org/pypi/grin for is_binary()
 
-inspired:
------------------
-  * grep-ack - a perl grep-like
-  * grin from http://pypi.python.org/pypi/grin for is_binary()
-
-TODO
+*TODO*
   - speed up, grep and ack-grep is faster
   - find in type (file extension, c, h, mk)
   - good ASCII icon
 
-FURTURE - maybe
+*FURTURE*
   - regular expression
   - export to file/HTML
   - case insensative
 
-CHANGELOG
-whoyeah.py-2.0, 2009-11-16
- * refactor and change project name
- * solve bugs: 
-   - str.find('egg') >= 0
-   - ignore dirs
+*CHANGELOG*
+-----------
+whoyeah.py-2.0, 2009-11-18
+  * refactor and change project name
+  * solve bugs: 
+    - str.find('egg') >= 0
+    - ignore dirs
+  * add
+    - ignore case
+    - command parse options
+    - multiple match in one line
 mfind-1.0a, 2009-08-28
- * clear colorful() when is not 'TERM' environment
+  * clear colorful() when is not 'TERM' environment
 mfind-1.0,2009-05-05
- * done basic find
+  * done basic find
 """ 
 
 # for file open
 from __future__ import with_statement # This isn't required in Python 2.6
-import sys, os, string
+import sys, os, string, re
 
 
 ### config ###
@@ -58,59 +49,70 @@ TEXTCHARS = ''.join(map(chr, [7,8,9,10,12,13,27] + range(0x20, 0x100)))
 ALLBYTES = ''.join(map(chr, range(256)))
 
 # main 
-def main():
-    target_dir = '.'
-    find_files = 0
-    sum_of_files = 0
-    sum_of_occurs = 0
-    if len(sys.argv) == 3:
-        target_dir = str(sys.argv[2]) 
+def main(str_pattern, target_dir):
+    num_files = 0
+    num_match_files = 0
+    num_matches = 0
 
     for path, dirs, files in os.walk(target_dir, topdown=True):
         for f in files:
-            # ignore the dirs we don't want to find
+            found = 0
             ignore = False
+            # if path have ignore directorys, skip to next
             for dirt in IGNORE_DIRS:
                 if path.find(dirt) > 0: 
                     ignore = True
                     break
             if not ignore:
-                found = find_in_file(f, path)
-                if found > 0 : sum_of_files += 1
-                find_files += 1
-                sum_of_occurs += found
+                found = find_in_file(f, path, str_pattern)
+                if found > 0 :
+                    num_match_files += 1
+                num_files += 1
+                num_matches += found
 
     print 'Summary:'
-    print colorful('  find in '+str(find_files)+' files', str(find_files),94)
-    print colorful('  occurs in '+str(sum_of_files)+' files', str(sum_of_files), 94) + colorful(', '+str(sum_of_occurs)+' times.', str(sum_of_occurs), 94)
+    print '  match', set_color(num_matches, 94), 'times, in', set_color(num_match_files, 94), 'files.'
+    print '  total find', set_color(num_files, 94), 'files'
 
 
-def find_in_file(f, path):
+def find_in_file(f, path, str_pattern):
     full_path = os.path.join(path, f)
+    re_flag = False
     found = 0
     ln = 0 # line number
-
+                
     with open(full_path) as fopen:
         if is_binary(full_path) != 1 and fopen not in IGNORE_FILES:
+            found = 0
             # find in each line
             for line in fopen:
+                match = []
                 ln += 1
-                if str(line).find(STR_PATTERN) >= 0:
+                if options.ignore_case:
+                    re_flag = re.IGNORECASE
+                for m in re.finditer(str_pattern, str(line), re_flag):
+                    match.append(m.group(0))
+                if len(match) > 0:
                     if found == 0:
-                        # print file name
-                        print colorful(full_path, full_path, 100) 
-                    found += 1
-                    # print line number and string, YELLOW and PURPLE!
-                    print colorful(str(ln), str(ln), 93) + ': ' + colorful(line[:-1], STR_PATTERN, 95)
+                        print set_color(full_path, 100)
+                    for keyword in match:
+                        line = replace_by_color(line, keyword, 95)
+                        found += 1
+                    print set_color(ln, 93) + ': ' + line[:-1]
+
         return found
 
 
-def colorful(obeh, colorize, code):
-    if os.getenv('TERM',None) in ['rxvt','xterm']:
-        return obeh.replace(colorize, '\033[0;'+ str(code) +'m' + colorize + '\033[m')
-    else:
-        return obeh
+def replace_by_color(string, keyword, color_code):
+    return string.replace(str(keyword), set_color(str(keyword),95) )
 
+
+def set_color(keyword, color_code):
+    if os.getenv('TERM',None) in ['rxvt','xterm']:
+        #return keyword.replace(str(keyword), '\033[0;'+str(color_code)+'m'+str(keyword)+'\033[m')
+        return '\033[0;' + str(color_code) + 'm' + str(keyword) + '\033[m'
+    else:
+        return string
 
 def is_binary(filename):
     fopen = open(filename, 'rb')
@@ -130,12 +132,19 @@ def is_binary_string(bytes):
 
 
 if __name__ == '__main__':
-    # usage
-    if len(sys.argv) < 2:
-        cmd = sys.argv[0].split('/')[-1]
-        print 'WhoYeah string finder, by MooGoo'
-        print 'Usage: ' + cmd + ' <STRING> [TARGET_DIR]'
-        sys.exit(1)
+    # parse options
+    from optparse import OptionParser
+    usage = "usage: %prog [options] PATTERN"
+    parser = OptionParser(usage=usage)
+    parser.add_option("-i", "--ignore_case",
+                      action='store_true', dest='ignore_case', default=False,
+                      help='ignore case sensitive')
+    parser.add_option('-t', '--target', dest='target_dir', default='.',
+                      help='set search target', metavar='TARGET')    
+    (options, args) = parser.parse_args()
+
+    if not args:
+        parser.print_help()
     else:
-        STR_PATTERN = str(sys.argv[1])
-        main()
+        str_pattern = args[0]
+        main(str_pattern, options.target_dir)
